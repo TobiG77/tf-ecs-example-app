0457684725722ff023b77d2ab3b69e18f3295baf
Tobias Gerschner <tobias@deepthought>
2017-11-16 12:42:44 +1300

    make it trivial to rerun terraform init, which may happen if a module needs a new provider

--- src/terraform/init.sh
+++ src/terraform/init.sh
@@ -43,27 +43,23 @@ set -e
 [ -n "$AWS_PROFILE" ] && export AWS_PROFILE
 [ -n "$APP_NAMESPACE" ] || abort_on_error "Need APP_PREFIX to be set"
 
-STATE_BUCKET="$APP_NAMESPACE-$STAGE-tf-state"
+export STATE_BUCKET="$APP_NAMESPACE-$STAGE-tf-state"
+export TF_VAR_region=${AWS_REGION}
+export TF_VAR_app_namespace="${APP_NAMESPACE}"
+export TF_VAR_stage=${STAGE}
+export AWS_PROFILE=${AWS_PROFILE}
+
+mkdir -p ../config
+envsubst < tf.env.tpl > ../config/tf-${STAGE}.env
 
 aws s3api head-bucket --bucket "$STATE_BUCKET" 2>/dev/null || create_state_bucket
 aws s3api get-bucket-lifecycle --bucket "$STATE_BUCKET" 2>/dev/null || put_bucket_lifecycle
 aws s3api get-bucket-versioning --bucket "$STATE_BUCKET" 2>/dev/null || put_bucket_versioning
 
-TF_VAR_region="${AWS_REGION}" \
-TF_VAR_app_namespace="${APP_NAMESPACE}" \
 terraform init \
   -get=true \
   -get-plugins=true \
   -verify-plugins=true \
   -backend-config="bucket=$STATE_BUCKET" \
-  -backend-config="key=$STAGE/terraform.tfstate" \
-  -backend-config="region=$AWS_REGION"
-
-mkdir -p ../config
-
-
-TF_VAR_region=${AWS_REGION} \
-TF_VAR_app_namespace="${APP_NAMESPACE}" \
-TF_VAR_stage=${STAGE} \
-AWS_PROFILE=${AWS_PROFILE} \
-envsubst < tf.env.tpl > ../config/tf-${STAGE}.env
+  -backend-config="key=$TF_VAR_stage/terraform.tfstate" \
+  -backend-config="region=$TF_VAR_region"
--- src/terraform/tf.env.tpl
+++ src/terraform/tf.env.tpl
@@ -1,4 +1,6 @@
 export AWS_PROFILE=${AWS_PROFILE}
+export STATE_BUCKET=${STATE_BUCKET}
+
 export TF_VAR_region=${TF_VAR_region}
 export TF_VAR_stage=${TF_VAR_stage}
 export TF_VAR_app_namespace="${TF_VAR_app_namespace}"
